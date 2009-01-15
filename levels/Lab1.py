98b10a2c5f056861ba1c46a757c41f9eab84ad44
nrackliffe <nrackliffe@bdd19a9c-b64e-11dd-b57e-3da4880f9b96>
2009-01-15 19:03:05 +0000

    Here's lab 1

---
+++ levels/Lab1.py
@@ -0,0 +1,27 @@
+import ogre.renderer.OGRE as ogre
+import ogre.physics.OgreOde as OgreOde
+import ogre.sound.OgreAL as OgreAL
+import ogre.io.OIS as OIS
+
+from Container import *
+
+class Level(BaseLevel):
+#    def __init__(self, levelId):
+#        BaseLevel.__init__(self, levelId)
+##
+##
+##    def __del__(self):
+##        BaseLevel.__del__(self)
+##
+
+    def load(self, app):
+        print 'Loaded Level Bob'
+
+        self.backgroundMusic = app.music['bg-3']
+
+        crate = makeCrate(app, 'Lab1 Crate', self.offset + ogre.Vector3(0, 2, 0))
+        crate.sound = app.sounds['bell-lo-0']
+
+
+    def unload(self, app):
+        pass
