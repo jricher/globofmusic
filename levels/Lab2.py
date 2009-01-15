9dae7b91fe46a23b225a21759677900aaef847cb
nrackliffe <nrackliffe@bdd19a9c-b64e-11dd-b57e-3da4880f9b96>
2009-01-15 19:12:55 +0000

    Lab2 (Platform)

--- levels/Lab2.py
+++ levels/Lab2.py
@@ -15,12 +15,15 @@ class Level(BaseLevel):
 ##
 
     def load(self, app):
-        print 'Loaded Level Lab1'
+        print 'Loaded Level Lab2'
 
         self.backgroundMusic = app.music['bg-3']
 
-        crate = makeCrate(app, 'Lab1 Crate', self.offset + ogre.Vector3(0, 2, 0))
+        crate = makeCrate(app, 'Lab2 Crate', self.offset + ogre.Vector3(0, 2, 0))
         crate.sound = app.sounds['bell-lo-0']
+        
+        platform = makeTiltingPlatform(app, "Lab2 Platform", self.offset + ogre.Vector3(-5,5,0) )
+        platform.sound = app.sounds['neutron-0']
 
 
     def unload(self, app):
